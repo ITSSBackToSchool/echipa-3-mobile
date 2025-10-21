ed3e248255f5886a835940092b4c8da1713c36bb
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-10-21 16:58:26 +0300

    add seats tab and make seats clickable

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -7,4 +7,9 @@ class AppColors {
   static const Color appBarGradientEnd = Color(0xFF4B5563);
   static const Color accent = Color(0xFFF59E0B);
   static const Color albastruDeschis = Color(0xFFE0F2FE);
+
+  // Culori pentru starea scaunelor
+  static const Color verde = Color(0xFF22C55E);
+  static const Color rosu = Color(0xFFEF4444);
+  static const Color portocaliu = Color(0xFFF97316);
 }
