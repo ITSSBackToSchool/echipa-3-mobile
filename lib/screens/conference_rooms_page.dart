bbac22ad857b42d92d2d1bdd1dc4f12a52866ec3
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-10-23 13:32:22 +0300

    add card buttons

--- lib/screens/conference_rooms_page.dart
+++ lib/screens/conference_rooms_page.dart
@@ -172,8 +172,6 @@ class _ConferenceRoomsPageState extends State<ConferenceRoomsPage>
                 height: 80,
                 fit: BoxFit.cover,
 
-
-
                 loadingBuilder: (context, child, progress) {
                   if (progress == null) return child;
                   return Container(
